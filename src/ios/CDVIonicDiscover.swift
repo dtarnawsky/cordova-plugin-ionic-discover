d4269b1cfedcb4f2469fed257f0890dad268a593
Damian Tarnawsky <damian@ionic.io>
2023-02-22 07:15:52 -0800

    fix: listen

--- src/ios/CDVIonicDiscover.swift
+++ src/ios/CDVIonicDiscover.swift
@@ -14,7 +14,7 @@ import Foundation
         // unwatch previous connections
         stop(nil)
 
-        service.start { }
+        service.listen()
         if let c = command {
             commandDelegate?.send(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: c.callbackId)
         }
